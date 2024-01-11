a95481be241be6a9a13cd69b6a9bd3eda4ff90bf
Kipchumba Lawrence <biwotlawrence@gmail.com>
2024-01-11 20:33:20 +0300

    Daily code work

--- app/Http/Controllers/Payment.php
+++ app/Http/Controllers/Payment.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class Payment extends Controller
 {
+    public $authToken;
 public function initialToken()
-{
+{   
     try {
        $curl = curl_init();
        curl_setopt_array($curl, array(
@@ -50,13 +50,14 @@ public function initialToken()
 }
 
 public function stkPush(){
-    $this->authToken();
+    $this->auth_token();
+    $password = base64_encode("222111" . "stkpushclient" . now()->format('YmdHis'));
 
     // STK Push Code
     $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'sandbox.familybank.co.ke:1044/api/v1/Mpesa/stkpush',
+  CURLOPT_URL => 'https://sandbox.familybank.co.ke:1044/api/v1/Mpesa/stkpush',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
@@ -71,22 +72,22 @@ curl_setopt_array($curl, array(
   "CallBackUrl": "https://dev.preshamafeedsltd.com",
   "PartyA": "254727750214",
   "PartyB": "222111",
-  "Password": "<string>",
+  "Password": "' . $password . '",
   "PhoneNumber": "254727750214",
   "ThirdPartyTransId": "Order0080",
-  "Timestamp": "<string>",
+  "Timestamp": "'.now()->format('YmdHis').'",
   "TransactionDesc": "Payment for the purchase of goods.",
   "TransactionType": "CustomerPayBillOnline"
 }',
   CURLOPT_HTTPHEADER => array(
     'Content-Type: application/json',
-    'Accept: text/plain',
-    'Authorization: Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjU2YzcwMjVmMjg4YTBlYWI1NDdmYTY2YmZlNDMwNTUzIiwidHlwIjoiSldUIn0.eyJuYmYiOjE2OTk5NTkzNjIsImV4cCI6MTY5OTk2Mjk2MiwiaXNzIjoiaHR0cHM6Ly9zYW5kYm94LmZhbWlseWJhbmsuY28ua2UiLCJhdWQiOlsiaHR0cHM6Ly9zYW5kYm94LmZhbWlseWJhbmsuY28ua2UvcmVzb3VyY2VzIiwiRVNCX1JFU1RfQVBJIl0sImNsaWVudF9pZCI6IlByZXNoYW1hIiwic2NvcGUiOlsiRVNCX1JFU1RfQVBJIl19.Yu90DM-7GsDqQ_vz3OqTN4pmIt_t1KjKStzs1QIGrtJpTKb3YOqPpYjs-DFYTmxTTtH-logN5WnVG7po02i6CWTgIbc2eWeDIkxEa_rwKx0RMWxX1ejaKUYWayk1HQQRYKFKkGvK_UmlNa0pv9uvMZmTLOrGsiXPxTyF33NaQ_EcXz4zsGBjSFK5S3Pgt7RcZJPebU7Ig1efxe13BSB9J6N2i3Ud9Iiyb-0mh9JYglTGyoUWS0CHShMsrMK3ZEkMZua8dhiFQe_tCmSfbXEZ6mBWC-puMqVRabU-leG-yrJclEawqpSnTTeDupSKviLg463w-qenvUDHbBMGIK26rg'
+    'Accept: application/json',
+    'Authorization: Bearer '.$this->authToken,
   ),
 ));
 
 $response = curl_exec($curl);
-
+dd($response);
 curl_close($curl);
 echo $response;
 
@@ -109,5 +110,5 @@ public function auth_token()
         CURLOPT_HTTPHEADER => array(    'Content-Type: application/x-www-form-urlencoded'  ),));
         $response = curl_exec($curl);curl_close($curl);
         $responseData = json_decode($response, true);
-        dd($responseData['access_token']);
+        $this->authToken=$responseData['access_token'];
 }}
